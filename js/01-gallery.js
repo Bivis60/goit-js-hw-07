784bef6be81ba8c7dd5988c1f2220bc499595f88
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-02-18 02:13:42 +0200

    Д.з. 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+const genGalleryItem = galleryItems.map((item) => {
+  return (
+`<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`)
+}).join('')
+gallery.insertAdjacentHTML('beforeend', genGalleryItem);
+
+
+
+gallery.addEventListener('click', onOpenModal);
+
+function onOpenModal (event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') return;
+    const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+    `)
+    instance.show();
+    window.addEventListener('keydown', onCloseModal);
+    function onCloseModal (event) {
+      if (event.code !== 'Escape') return;
+      instance.close()
+      window.removeEventListener('keydown', onCloseModal);
+    }
+    
+}
 console.log(galleryItems);
